03f3b4e6024d5d91d0e5486678b6b705e279b71a
WangYue <516898428@qq.com>
2023-09-19 15:10:38 +0800

    netty中的hello world

---
+++ chaosrpc/demo/src/main/java/com/chaos/netty/AppClient.java
@@ -0,0 +1,58 @@
+package com.chaos.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class AppClient {
+
+    public void run() {
+        // 定义线程池，EventLoopGroup
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        // 启动一个客户端需要一个辅助类，bootstrap
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            bootstrap = bootstrap.group(group)
+                    .remoteAddress(new InetSocketAddress(8080))
+                    // 选择初始化一个什么样的channel
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new MyChannelHandler2());
+                        }
+                    });
+            // 尝试连接服务器
+            ChannelFuture channelFuture = null;
+            channelFuture = bootstrap.connect().sync();
+            // 获取channel，并且写出数据
+            channelFuture.channel()
+                    .writeAndFlush(
+                            Unpooled.copiedBuffer("hello netty".getBytes(StandardCharsets.UTF_8))
+                    );
+            // 阻塞程序，等待接收消息
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new AppClient().run();
+    }
+}
