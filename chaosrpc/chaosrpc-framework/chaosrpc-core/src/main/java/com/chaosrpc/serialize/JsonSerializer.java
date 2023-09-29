5eb2f4732553df82c358d549cf9851dc036724b3
WangYue <516898428@qq.com>
2023-09-29 10:10:57 +0800

    Json序列化，未解决 Json 不支持 ClassForName

--- chaosrpc/chaosrpc-framework/chaosrpc-core/src/main/java/com/chaosrpc/serialize/JsonSerializer.java
+++ chaosrpc/chaosrpc-framework/chaosrpc-core/src/main/java/com/chaosrpc/serialize/JsonSerializer.java
@@ -1,15 +1,35 @@
 package com.chaosrpc.serialize;
 
-import java.io.IOException;
+import com.alibaba.fastjson2.JSON;
+import com.chaos.exceptions.SerializeException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+@Slf4j
 public class JsonSerializer implements Serializer{
     @Override
     public byte[] serialize(Object object) throws IOException {
-        return new byte[0];
+        if(object == null) {
+            return null;
+        }
+        byte[] bytes = JSON.toJSONBytes(object);
+        if(log.isDebugEnabled()) {
+            log.debug("对象{}使用json已经完成了序列化.", object);
+        }
+        return bytes;
     }
 
     @Override
     public <T> T disSerialize(byte[] bytes, Class<T> clazz) {
-        return null;
+        if(bytes == null || clazz == null) {
+            return null;
+        }
+        T t = JSON.parseObject(bytes, clazz);
+        if(log.isDebugEnabled()) {
+            log.debug("类{}使用json已经完成了反序列化.", clazz);
+        }
+        return (T)t;
     }
 }
